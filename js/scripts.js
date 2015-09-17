4089d3f53c3ab891e5b45c5c350203113e93d3ad
Mitch Rivet & Kevin Tokheim <student@epicodus.com>
2015-09-17 11:33:49 -0700

    add basic play rules

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,6 @@ var spaceArray = ["", "", "",
                   "", "", "",
                   "", "", ""];
 
-console.log(spaceArray);
 function Space(x_coordinate, y_coordinate) {
   this.x_coordinate = x_coordinate;
   this.y_coordinate = y_coordinate;
@@ -58,58 +57,122 @@ Space.prototype.marked_by = function() {
 
 
 
+
+
 $(document).ready(function() {
 
 
+
   var userprompt = prompt("Welcome to Tic Tac Toe! Would you like to be X or O?");
+
   if (userprompt.toLowerCase() == "x") {
     alert("You are player x!");
-      var inputtedMark = "X";
-  } else if (userprompt.toLowerCase() == "y") {
-    alert("You are player y!");
-    var inputtedMark = "Y";
+      var playerOneMark = "X";
+      var playerTwoMark = "O";
+  } else if (userprompt.toLowerCase() == "o") {
+    alert("You are player O!");
+    var playerTwoMark = "X";
+    var playerOneMark = "O";
   } else {
-    prompt("Please enter an X or Y");
+    prompt("Please enter an X or O");
   }
+
+  // function setTurn() {
+  //   var r =
+  // }
 
   $(".hiddenDiv").show();
 
+  var counter = 0;
+  $(".ticTacToe").click(function()
+  {
+    counter++;
+          if (counter % 2 === 0)
+          {
 
-  $(".one_one").click(function() {
-    $(this).text(inputtedMark);
-  });
+            $(this).text(playerTwoMark);
+            $(this).unbind();
 
-  $(".one_two").click(function() {
-    $(".one_two").text(inputtedMark);
-  });
+          } else
+          {
 
-  $(".one_three").click(function() {
-    $(".one_three").text(inputtedMark);
-  });
+            $(this).text(playerOneMark);
+            $(this).unbind();
+          }
 
-  $(".two_one").click(function() {
-    $(".two_one").text(inputtedMark);
-  });
 
-  $(".two_two").click(function() {
-    $(".two_two").text(inputtedMark);
-  });
+          if ($("#one_three").text() == "X" && $("#two_three").text() == "X" && $("#three_three").text() == "X" ||
+          $("#one_two").text() == "X" && $("#two_two").text() == "X" && $("#three_two").text() == "X" ||
+          $("#one_one").text() == "X" && $("#two_one").text() == "X" && $("#three_one").text() == "X" ||
+          $("#one_one").text() == "X" && $("#one_two").text() == "X" && $("#one_three").text() == "X" ||
+          $("#two_one").text() == "X" && $("#two_two").text() == "X" && $("#two_three").text() == "X" ||
+          $("#three_one").text() == "X" && $("#three_two").text() == "X" && $("#three_three").text() == "X" ||
+          $("#one_three").text() == "X" && $("#two_two").text() == "X" && $("#three_one").text() == "X" ||
+          $("#one_one").text() == "X" && $("#two_two").text() == "X" && $("#three_three").text() == "X")
+          {
+            alert("Player one wins!");
+          } else if ($("#one_three").text() == "O" && $("#two_three").text() == "O" && $("#three_three").text() == "O" ||
+          $("#one_two").text() == "O" && $("#two_two").text() == "O" && $("#three_two").text() == "O" ||
+          $("#one_one").text() == "O" && $("#two_one").text() == "O" && $("#three_one").text() == "O" ||
+          $("#one_one").text() == "O" && $("#one_two").text() == "O" && $("#one_three").text() == "O" ||
+          $("#two_one").text() == "O" && $("#two_two").text() == "O" && $("#two_three").text() == "O" ||
+          $("#three_one").text() == "O" && $("#three_two").text() == "O" && $("#three_three").text() == "O" ||
+          $("#one_three").text() == "O" && $("#two_two").text() == "O" && $("#three_one").text() == "O" ||
+          $("#one_one").text() == "O" && $("#two_two").text() == "O" && $("#three_three").text() == "O")
+          {
+            alert("Player two wins!");
+          }
+    });
 
-  $(".two_three").click(function() {
-    $(".two_three").text(inputtedMark);
-  });
 
-  $(".three_one").click(function() {
-    $(".three_one").text(inputtedMark);
-  });
 
-  $(".three_two").click(function() {
-    $(".three_two").text(inputtedMark);
-  });
+    // if ($(".one_three").text() == "X" && $(".two_three").text() == "X" && $(".three_three").text() == "X")
+    // {
+    //   alert("Player one wins!");
+    // }
 
-  $(".three_three").click(function() {
-    $(".three_three").text(inputtedMark);
-  });
+
+
+
+  // $(".one_one").click(function() {
+  //   $(this).text(getMark());
+  // });
+  //
+  // $(".one_two").click(function() {
+  //   $(".one_two").text(getMark());
+  // });
+  //
+  // $(".one_three").click(function() {
+  //   $(".one_three").text(getMark());
+  // });
+  //
+  // $(".two_one").click(function() {
+  //   $(".two_one").text(getMark());
+  // });
+  //
+  // $(".two_two").click(function() {
+  //   $(".two_two").text(getMark());
+  // });
+  //
+  // $(".two_three").click(function() {
+  //   $(".two_three").text(getMark());
+  // });
+  //
+  // $(".three_one").click(function() {
+  //   $(".three_one").text(getMark());
+  // });
+  //
+  // $(".three_two").click(function() {
+  //   $(".three_two").text(getMark());
+  // });
+  //
+  // $(".three_three").click(function() {
+  //   $(".three_three").text(getMark());
+  // });
+
+
+
+
 
 
 });
